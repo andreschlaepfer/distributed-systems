6c77283df5505c7b4e06adf63884135a8af68b5e
Andre Schlaepfer <andre.aranha7@gmail.com>
2022-05-03 00:56:56 -0300

    Merge branch 'features/Sockets' into develop

---
+++ Trabalho 1/Sockets/consumidor.c
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <errno.h>
+#include <math.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <strings.h>
+
+int CheckPrime(int n)
+{
+	if (n <= 1)
+	{
+		return 0;
+	}
+	for (int i = 2; i <= sqrt((double)n); i++)
+	{
+		if (n % i == 0)
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int main(int arg, char *argv[])
+{
+	struct sockaddr_in caddr;
+	struct sockaddr_in saddr = {
+			.sin_family = AF_INET,
+			.sin_addr.s_addr = htonl(INADDR_ANY),
+			.sin_port = htons(5000)};
+
+	int server = socket(AF_INET, SOCK_STREAM, 0);
+	bind(server, (struct sockaddr *)&saddr, sizeof saddr);
+	listen(server, 5);
+
+	int csize = sizeof caddr;
+	int cliente;
+	char buff[150];
+	int isPrime;
+	cliente = accept(server, (struct sockaddr *)&caddr, (unsigned int *)&csize);
+
+	while (true)
+	{
+		printf("Comecando a ouvir...\n");
+		bzero(buff, sizeof buff);
+		read(cliente, buff, sizeof buff);
+		if (atoi(buff) == 0)
+		{
+			printf("Terminando o programa...\n");
+			break;
+		}
+		printf("Mensagem recebida: %s\n", buff);
+		isPrime = CheckPrime(atoi(buff));
+		printf("Resultado obtido: %d\n", isPrime);
+		bzero(buff, sizeof buff);
+		sprintf(buff, "%d", isPrime);
+		write(cliente, buff, sizeof buff);
+	}
+	close(cliente);
+	return 0;
+}
---
+++ Trabalho 1/Sockets/produtor.c
@@ -0,0 +1,71 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <errno.h>
+#include <math.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <strings.h>
+
+void PrintResult(int result)
+{
+  if (result == 1)
+  {
+    printf("É primo!\n");
+  }
+  if (result == 0)
+  {
+    printf("Não é primo\n");
+  }
+}
+
+int RandomNumberGenerator(int n)
+{
+  return n + random() % 100 + 1;
+}
+
+int main(int arg, char *argv[])
+{
+  int n = 1;
+  int maxIterations = 10;
+  int numberOfIterations = 1;
+
+  struct sockaddr_in saddr = {
+      .sin_family = AF_INET,
+      .sin_addr.s_addr = htonl(INADDR_ANY),
+      .sin_port = htons(5000)};
+
+  int server = socket(AF_INET, SOCK_STREAM, 0);
+  if (connect(server, (struct sockaddr *)&saddr, sizeof saddr) != 0)
+  {
+    printf("Não foi possível estabelecer conexão\n");
+    exit(0);
+  }
+  printf("Conectado!\n");
+
+  char buff[150];
+
+  while (numberOfIterations <= maxIterations)
+  {
+    printf("%d: ", numberOfIterations);
+    bzero(buff, sizeof buff);
+    n = RandomNumberGenerator(n);
+    sprintf(buff, "%d", n);
+    printf("%s  ", buff);
+    write(server, buff, sizeof buff);
+    bzero(buff, sizeof buff);
+    read(server, buff, sizeof buff);
+    PrintResult(atoi(buff));
+    numberOfIterations++;
+  }
+  printf("Saindo do loop!\n");
+  n = 0;
+  bzero(buff, sizeof buff);
+  sprintf(buff, "%d", n);
+  write(server, buff, sizeof buff);
+  close(server);
+  return 0;
+}
