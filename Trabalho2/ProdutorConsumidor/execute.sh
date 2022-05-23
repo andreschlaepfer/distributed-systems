8361c7794229f4715036c7c1499e05a1df8b375d
Thiago Costa <thiagodias2708@gmail.com>
2022-05-22 22:21:46 -0300

    fix: producer consumer script

--- Trabalho2/ProdutorConsumidor/execute.sh
+++ Trabalho2/ProdutorConsumidor/execute.sh
@@ -7,7 +7,7 @@ gcc producer_consumer.c -o producer_consumer
 for K in "1 1" "1 2" "1 4" "1 8" "2 1" "4 1" "8 1"
   do
     printf "\n" >> $CSV_PATH
-    printf "$K" >> $CSV_PATH
+    printf "($K)" >> $CSV_PATH
     for N in 1, 2, 4, 16, 32
       do
         printf ",$(./producer_consumer $N $K)" >> $CSV_PATH
