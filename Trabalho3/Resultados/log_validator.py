2ef0b5548f228043bb34843a99ca6936d5cfcb15
Thiago <thiagodias2708@gmail.com>
2022-07-20 12:30:16 -0300

    fix: log validator

--- Trabalho3/Resultados/log_validator.py
+++ Trabalho3/Resultados/log_validator.py
@@ -2,10 +2,7 @@ import os
 
 def validate():
     dir = os.path.dirname(os.path.realpath(__file__))
-
     f = open(dir + "/log.txt", "r")
-    r = open(dir +"/resultado.txt", "r")
-    rLines = r.readlines()
     lines = f.readlines()
     requests = []
     grants = []
@@ -20,25 +17,20 @@ def validate():
                 print(line)
                 print(len(grants))
                 print(len(releases))
-                raise Exception(
-                    "1 Invalid log file: invalid grants and releases sequence")
+                raise Exception("Invalid log file: invalid grants and releases sequence")
             grants.append(int(line.split("-")[1]))
             continue
         if ("[R] Release" in line):
             if (len(releases) != len(grants) - 1):
-                raise Exception(
-                    "2 Invalid log file: invalid grants and releases sequence")
+                raise Exception("Invalid log file: invalid grants and releases sequence")
             releases.append(int(line.split("-")[1]))
             continue
 
     for i in range(len(requests)):
         if (requests[i] != grants[i] or grants[i] != releases[i]):
-            raise Exception(
-                "3 Invalid log file: invalid grants and releases sequence")
+            raise Exception("Invalid log file: invalid grants and releases sequence")
 
-    if(len(rLines) == 1280):
-        print("Log file was successfully validated")
-
+    print("Log file was successfully validated")
 
 if __name__ == "__main__":
     validate()
