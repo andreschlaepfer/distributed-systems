bd9aa3b06007a49eb38de311fc0a93908060035c
Andre Schlaepfer <aaschlaepfer@inoa.com.br>
2022-07-20 22:59:44 -0300

    chore

--- Trabalho3/Client/Run.cs
+++ Trabalho3/Client/Run.cs
@@ -1,33 +1,27 @@
-﻿namespace Client
-{
-  public class Run
-  {
-    public static void Main(string[] args)
-    {
-      if (args.Length != 3)
-      {
-        System.Console.WriteLine("\nErro:\n  Insira os argumentos corretamente: <n> <r> <k>");
-        return;
-      }
-      // arguments must be numbers
-      if (!int.TryParse(args[0], out int n) || !int.TryParse(args[1], out int r) || !int.TryParse(args[2], out int k))
-      {
-        System.Console.WriteLine("\nErro:\n  Argumentos inválidos");
-        return;
-      }
+﻿namespace Client {
+    public class Run {
+        public static void Main(string[] args) {
+            if (args.Length != 3) {
+                Console.WriteLine("\nErro:\n  Insira os argumentos corretamente: <n> <r> <k>");
+                return;
+            }
+            // arguments must be numbers
+            if (!int.TryParse(args[0], out int n) || !int.TryParse(args[1], out int r) || !int.TryParse(args[2], out int k)) {
+                Console.WriteLine("\nErro:\n  Argumentos inválidos");
+                return;
+            }
 
 
 
-      n = Convert.ToInt32(args[0]);
-      r = Convert.ToInt32(args[1]);
-      k = Convert.ToInt32(args[2]);
+            n = Convert.ToInt32(args[0]);
+            r = Convert.ToInt32(args[1]);
+            k = Convert.ToInt32(args[2]);
 
-      for (var i = 1; i <= n; i++)
-      {
-        var newClient = new Client(i, r, k);
-        var newThread = new Thread(newClient.Connect);
-        newThread.Start();
-      }
+            for (var i = 1; i <= n; i++) {
+                var newClient = new Client(i, r, k);
+                var newThread = new Thread(newClient.Connect);
+                newThread.Start();
+            }
+        }
     }
-  }
 }
